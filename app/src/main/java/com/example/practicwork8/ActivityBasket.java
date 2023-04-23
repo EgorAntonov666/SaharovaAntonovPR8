97eded5d0b77114619b265edc1631e251b0a0774
unknown <egorantonov1804@gmail.com>
2023-04-23 15:40:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/practicwork8/ActivityBasket.java
@@ -0,0 +1,14 @@
+package com.example.practicwork8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityBasket extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basket);
+    }
+}
